0cbf6ec223a54ff277216b9f2cd0a5e93b45a1b4
Javier López de Ancos <javierlopezdeancos@outlook.com>
2020-06-21 11:15:36 +0200

    feat: add build ci and perf types

--- install.sh
+++ install.sh
@@ -57,7 +57,7 @@ if [[ -n $1 ]] && [[ $1 == '--scripts' ]]; then
 else
   echo 'Installing git aliases…'
 
-  semantic_aliases=( 'chore' 'docs' 'feat' 'fix' 'localize' 'chore' 'refactor' 'style' 'test' )
+  semantic_aliases=( 'chore' 'docs' 'feat' 'fix' 'localize' 'chore' 'refactor' 'style' 'test' 'build' 'ci' 'perf' )
 
   for semantic_alias in "${semantic_aliases[@]}"; do
     register_git_alias $semantic_alias
